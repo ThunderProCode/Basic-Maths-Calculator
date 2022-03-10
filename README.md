43f769d951a3172829ff144d048cca7ec1e68b45
Hector Eddin Acosta Najera <hector889ju@gmail.com>
2022-03-10 12:18:07 -0600

    First Version done

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # js-course
-Exercise from a javascript course.
 
--Hector Acosta
+## Practice #1: Geometric figures
+
+- 1st step: Define the formulas
+- 2nd step: Implement formulas in javascript
+- 3rd step: create functions 
+- 4th step: Integrate JS with HTML
---
+++ figures.js
@@ -0,0 +1,75 @@
+//Square Code
+function squarePerimeter(squareSide) {
+    return squareSide * 4;
+};
+
+function squareArea(squareSide) {
+    return squareSide* squareSide;
+}
+
+//Triangle code
+function trianglePerimeter(triangleSide, triangleSide2, triangelBase) {
+    return triangleSide + triangleSide2 + triangelBase;
+}
+
+function triangleArea(triangleSide, triangleSide2, triangelBase) {
+    const sP = (triangleSide + triangleSide2 + triangelBase) / 2; 
+    return Math.sqrt(sP * (sP - triangleSide) * (sP - triangleSide2) * (sP - triangelBase));
+}
+
+//Circle Code
+function circleDiameter(radio){
+    return radio * 2;
+}
+
+function circlePerimeter(circleRadio) {
+    return circleDiameter(circleRadio) * Math.PI;
+}
+
+function circleArea(circleRadio) {
+    return (circleRadio * circleRadio) * Math.PI;
+}
+
+//Square
+function calculateSquarePerimeter() {
+    const input = document.getElementById("squareInput");
+    const value = input.value;
+    const perimeter = squarePerimeter(value);
+    document.getElementById("square-perimeter-answer").innerHTML = perimeter + "cm";
+}
+
+function calculateSquareArea() {
+    const input = document.getElementById("squareInput");
+    const value = input.value;
+    const area = squareArea(value);
+    document.getElementById("square-area-answer").innerHTML = area + "cm^2";
+}
+
+//Triangle
+function calculateTrianglePerimeter() {
+    const side1 = document.getElementById("triangleSide1Input");
+    const side2 = document.getElementById("triangleSide3Input");
+    const base = document.getElementById("triangleBaseInput");
+
+    const perimeter = trianglePerimeter(side1,side2,base);
+    alert(perimeter);
+}
+
+function calculateTriangleArea() {
+    const side1 = document.getElementById("triangleSide1Input");
+    const side2 = document.getElementById("triangleSide3Input");
+    const base = document.getElementById("triangleBaseInput");
+}
+
+//Circle 
+function calculateCirclePerimeter() {
+    const radio = document.getElementById("circleInput");
+    const perimeter = circlePerimeter(radio);
+    alert(perimeter);
+}
+
+function calculateCircleArea() {
+  const radio = document.getElementById("circleInput");
+  const area = circleArea(radio);
+  alert(area);
+}
