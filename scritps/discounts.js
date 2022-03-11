876148244a3004dea90856e2cbf6ab13290b6c9b
Hector Eddin Acosta Najera <hector889ju@gmail.com>
2022-03-11 09:03:27 -0600

    discounts and percentages page added

---
+++ scritps/discounts.js
@@ -0,0 +1,10 @@
+function getDiscount(price,discount){
+    const priceWithDiscount = ((price * (100 - discount))/100);
+    return priceWithDiscount;
+}
+
+console.log(getDiscount(180,15));
+
+function getPercentage(percentage, number){
+    
+}
